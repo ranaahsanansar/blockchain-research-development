7c56d45cdd2940db00d3940829b6b51913072645
ranaahsanansar <asn.cs21@gmail.com>
2024-03-14 16:34:52 +0500

    Add new Functions for Breeding

--- breeding-nft-usecase/contracts/breedingWith6059/BreedingNft.sol
+++ breeding-nft-usecase/contracts/breedingWith6059/BreedingNft.sol
@@ -4,25 +4,46 @@ pragma solidity ^0.8.16;
 
 import "erc6059-research/NestableToken.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "erc6059-research/NftGenericts.sol";
 
 //Minimal public implementation of IRMRKNestable for testing.
-contract BreedingNfts is NestableToken , NftGenericts {
-    constructor(address initialOwner) NestableToken() {
-        Ownable(initialOwner)
+contract BreedingNfts is NestableToken , Ownable {
+
+uint256 private _nextTokenId;
+
+    struct Parents {
+        uint256 intrestedToken;
+        uint256 partnerToken;
     }
 
-    function mint(address to, uint256 tokenId) external onlyOwner {
+    mapping(uint256 => Parents) public parentsRecord;
+
+    constructor(address initialOwner) NestableToken() Ownable(initialOwner) {
+        _nextTokenId = 1;
+    }
+
+    function mint(address to) external onlyOwner {
+        uint256 tokenId = _nextTokenId++;
+        parentsRecord[tokenId] = Parents(0, 0 );
         _mint(to, tokenId);
     }
 
-    function nestMint(
-        address to,
-        uint256 tokenId,
-        uint256 destinationId
-    ) external onlyOwner {
-        _nestMint(to, tokenId, destinationId, "");
+    function breedNft(uint256 intrestedToken , uint256 patnerToken, address intrestedTokenContractAddress ) external onlyOwner {
+        uint256 tokenId = _nextTokenId++;
+        _nestMint(intrestedTokenContractAddress, tokenId, intrestedToken, "");
+        parentsRecord[tokenId] = Parents(intrestedToken, patnerToken );
     }
+
+    function parentsOf(uint256 tokenId) external view returns (Parents memory _parent) {
+        return parentsRecord[tokenId];
+    }
+
+    // function nestMint(
+    //     address to,
+    //     uint256 tokenId,
+    //     uint256 destinationId
+    // ) external onlyOwner {
+    //     _nestMint(to, tokenId, destinationId, "");
+    // }
 
     // Utility transfers:
 
