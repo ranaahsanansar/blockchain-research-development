1ca4d2ef40d161641ae24daf4ded39aebe6ec1c6
ranaahsanansar <asn.cs21@gmail.com>
2024-03-15 12:01:52 +0500

    Implementation breeding ERC6551

--- breeding-nft-usecase/contracts/breedingWith6551/BreedingToken.sol
+++ breeding-nft-usecase/contracts/breedingWith6551/BreedingToken.sol
@@ -1,27 +1,50 @@
 // SPDX-License-Identifier: MIT
-// Compatible with OpenZeppelin Contracts ^5.0.0
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./NftGenetics.sol";
+import "./OyoCoin.sol";
 
-contract BreedingToken is ERC721, NftGenetics, Ownable {
+contract BreedingToken is ERC721, Genetics, Ownable {
     uint256 private _nextTokenId;
+    address coinContractAddress;
 
     constructor(
-        address initialOwner
-    ) ERC721("MyToken", "MTK") Ownable(initialOwner) {}
+        address initialOwner,
+        address coinAddress
+    ) ERC721("Elysium World", "EWT") Ownable(initialOwner)  {
+        coinContractAddress = coinAddress;
+    }
 
-    function safeMint(address to) public onlyOwner {
+   
+
+    function updateCoinAddress(address newAddress) public onlyOwner {
+        coinContractAddress = newAddress;
+    }
+
+    function safeMint(address to , string memory uri) public onlyOwner {
         uint256 tokenId = _nextTokenId++;
         _safeMint(to, tokenId);
+        _setNftGenetics(tokenId , NftGenetics(0, block.timestamp, uri));
     }
 
-    function setNftGenetics(
-        uint256 tokenId,
-        NftGenetics memory genetics
-    ) public onlyOwner {
-        _setNftGenetics(tokenId, genetics);
+    function setOrUpdateBreedingPrice(uint256 price , uint256 tokenId) external {
+        require(ownerOf(tokenId) == _msgSender(), "Only Owner can set the price");
+        _setBreedingPrice(price , tokenId);
     }
+
+    function breedTokens(uint256 intrestedToken, uint256 partnerToken, string memory uriStorage ) public onlyOwner returns(bool){
+        require(_checkBreedingStatus( partnerToken) == true , "Partner is underage");
+        require(_checkBreedingStatus( intrestedToken) == true , "Intrested token is underage");
+        bool transferStatus = OyoCoin(coinContractAddress).transferFrom(ownerOf(intrestedToken) , ownerOf(partnerToken) , _getBreedingPrice(partnerToken));
+        if(transferStatus == true){
+            uint256 tokenId = _nextTokenId++;
+            _safeMint(ownerOf(intrestedToken), tokenId);
+        _setNftGenetics(tokenId , NftGenetics(0, block.timestamp, uriStorage));
+        }
+        return true; 
+    }
+
+
 }
--- breeding-nft-usecase/contracts/breedingWith6551/NftGenetics.sol
+++ breeding-nft-usecase/contracts/breedingWith6551/NftGenetics.sol
@@ -2,21 +2,43 @@
 // SPDX-License-Identifier: CC0-1.0
 pragma solidity ^0.8.16;
 
-abstract contract NftGenericts {
+abstract contract Genetics {
+
+  uint256 ageLimit;
   struct NftGenetics {
     uint256 breedingPrice;
-    string geneticsUri;
+    uint256 dateOfBirth;
     string tokenUri;
   }
 
-  mapping(uint256 => NftGenetics) public nftGenetics;
 
+  mapping(uint256 => NftGenetics) internal nftGenetics;
 
-  function setNftGenetics(uint256 tokenId, NftGenetics memory genetics) internal {
+  function _setAgeLimit(uint256 _ageLimit) internal {
+    ageLimit = _ageLimit;
+  }
+
+  function _setNftGenetics(uint256 tokenId, NftGenetics memory genetics) internal {
     nftGenetics[tokenId] = genetics;
   }
 
   function getNftGenetics(uint256 tokenId) external view returns (NftGenetics memory) {
     return nftGenetics[tokenId];
+  }
+
+  function _setBreedingPrice(uint256 _price , uint256 tokenId) internal {
+    nftGenetics[tokenId].breedingPrice = _price;
+  }
+
+  function _checkBreedingStatus(uint256 tokenId) internal view returns (bool) {
+    if(nftGenetics[tokenId].dateOfBirth + ageLimit <= block.timestamp ){
+      return true;
+    }else {
+      return false;
+    }
+  }
+
+  function _getBreedingPrice(uint256 tokenId) internal view returns (uint256 ){
+    return nftGenetics[tokenId].breedingPrice;
   }
 }
