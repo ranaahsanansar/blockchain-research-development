87276c623506a280f49b1137411481340fd451d3
ranaahsanansar <asn.cs21@gmail.com>
2024-05-03 10:56:34 +0500

    Add Hacking Contracts

--- erc6551-token-bound-account/contracts/721MyToken.sol
+++ erc6551-token-bound-account/contracts/721MyToken.sol
@@ -2,12 +2,40 @@
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MYERC721 is ERC721, Ownable {
-    constructor() ERC721("MyToken", "MTK") {}
+contract Cars is ERC721, ERC721URIStorage, Ownable {
+    uint256 private _nextTokenId;
 
-    function safeMint(address to, uint256 tokenId) public onlyOwner {
-        _safeMint(to, tokenId);
+    constructor(address initialOwner)
+        ERC721("Rebel Cars", "RCN")
+        Ownable(initialOwner)
+    {}
+
+    function mint(address to, string memory uri) public onlyOwner {
+        uint256 tokenId = _nextTokenId++;
+        _mint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
     }
 }
